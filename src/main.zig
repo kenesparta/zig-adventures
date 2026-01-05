23a8bbc6d9ae09513655bd33999fa8f961e70885
Ken Esparta <kenesparta@pm.me>
2026-01-04 19:22:27 -0500

    feat: adding config and rectangle structs

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,44 @@
 const rl = @import("raylib");
 
+const Rectangle = struct {
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+
+    /// We can use this method something like this:
+    /// rect1.intersects(rect2)
+    /// or:
+    /// Rectangle.intersects(rect1, rect2)
+    pub fn intersects(self: Rectangle, other: Rectangle) bool {
+        return self.x < other.x + other.width and
+            self.x + self.width > other.x and
+            self.y < other.y + other.height and
+            self.y + self.height > other.y;
+    }
+};
+
+const GameConfig = struct {
+    screenWith: i32,
+    screenHeight: i32,
+    playerWidth: f32,
+    playerHeight: f32,
+    playerStartY: f32,
+    bulletWidth: f32,
+    bulletHeight: f32,
+    shieldStartX: f32,
+    shieldY: f32,
+    shieldWidth: f32,
+    shieldHeight: f32,
+    shieldSpacing: f32,
+    invaderStartX: f32,
+    invaderStartY: f32,
+    invaderWidth: f32,
+    invaderHeight: f32,
+    invaderSpacingX: f32,
+    invaderSpacingY: f32,
+};
+
 pub fn main() void {
     const screen_with = 800;
     const screen_height = 800;
