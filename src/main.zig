e0d4ed28c8ba21374f3bee71b2b65019ed642ef9
Ken Esparta <kenesparta@pm.me>
2026-01-12 00:23:38 -0500

    feat: adding Player with constructor

--- src/main.zig
+++ src/main.zig
@@ -19,32 +19,82 @@ const Rectangle = struct {
 };
 
 const GameConfig = struct {
-    screenWith: i32,
-    screenHeight: i32,
-    playerWidth: f32,
-    playerHeight: f32,
-    playerStartY: f32,
-    bulletWidth: f32,
-    bulletHeight: f32,
-    shieldStartX: f32,
-    shieldY: f32,
-    shieldWidth: f32,
-    shieldHeight: f32,
-    shieldSpacing: f32,
-    invaderStartX: f32,
-    invaderStartY: f32,
-    invaderWidth: f32,
-    invaderHeight: f32,
-    invaderSpacingX: f32,
-    invaderSpacingY: f32,
+    screen_width: i32,
+    screen_height: i32,
+    player_width: f32,
+    player_height: f32,
+    player_start_y: f32,
+    bullet_width: f32,
+    bullet_height: f32,
+    shield_start_x: f32,
+    shield_y: f32,
+    shield_width: f32,
+    shield_height: f32,
+    shield_spacing: f32,
+    invader_start_x: f32,
+    invader_start_y: f32,
+    invader_width: f32,
+    invader_height: f32,
+    invader_spacing_x: f32,
+    invader_spacing_y: f32,
+};
+
+// @This = builtin Function, is the same as Example
+const Example = struct {
+    value: i32,
+    /// init is a convention for constructor functions
+    pub fn init(starting_value: i32) @This() {
+        return .{
+            .value = starting_value,
+        };
+    }
+
+    // @This() -> Read only
+    // *@This() -> Write (modify)
+    pub fn update(self: *@This()) void {
+        self.value += 1;
+    }
+};
+
+const Player = struct {
+    position_x: f32,
+    position_y: f32,
+    width: f32,
+    height: f32,
+    speed: f32,
+
+    pub fn init(
+        position_x: f32,
+        position_y: f32,
+        width: f32,
+        height: f32,
+    ) @This() {
+        return .{
+            .position_x = position_x,
+            .position_y = position_y,
+            .width = width,
+            .height = height,
+            .speed = 5.0,
+        };
+    }
 };
 
 pub fn main() void {
-    const screen_with = 800;
+    const screen_width = 800;
     const screen_height = 800;
 
-    rl.initWindow(screen_with, screen_height, "Zig Invaders");
+    rl.initWindow(screen_width, screen_height, "Zig Invaders");
     defer rl.closeWindow();
+
+    const player_width = 50.0;
+    const player_height = 30.0;
+
+    var player = Player.init(
+        @as(f32, @floatFromInt(screen_width)) / 2 - player_width / 2,
+        @as(f32, @floatFromInt(screen_height)) - 60.0,
+        player_width,
+        player_height,
+    );
 
     rl.setTargetFPS(60);
     while (!rl.windowShouldClose()) {
